95d7e1d2cd63e857dfb2754eaeef97627e75383b
zachalbers <back2zach2@gmail.com>
2019-03-24 23:43:52 -0600

    Newest Update

--- indexFile.js
+++ indexFile.js
@@ -18,3 +18,44 @@ for (var i = 0; i <allCards.length; i++) {
     this.classList.add('shadow-sm');
   });
 }
+
+
+
+
+
+// Get the modal
+
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var imgs = document.querySelectorAll(".hoverImage");
+var modal = document.querySelector(".hoverModal");
+
+
+var modalImg = document.querySelector(".hoverModal-content");
+var captionText = document.querySelectorAll(".hoverModal-caption");
+var spans = document.querySelectorAll(".HoverImageclose");
+
+
+
+// console.log(modals.length)
+// console.log(modalImgs.length)
+
+
+for (var i = 0; i <imgs.length; i++) {
+
+
+  imgs[i].onclick = function(){
+
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  spans[i].onclick = function() {
+    modal.style.display = "none";
+  }
+}
+
+// Get the <span> element that closes the modal
+
+
+// When the user clicks on <span> (x), close the modal
